f2300cd1ebcb451b558a8c880da2ceef0352b724
Angi Munoz <angiemar-12@hotmail.com>
2023-03-06 17:16:49 -0500

    change in view card

--- src/views/Board.jsx
+++ src/views/Board.jsx
@@ -8,10 +8,11 @@ import Layout from "../components/layout/Layout";
 
 export default function Board() {
   return (
+    <div id="board" className="h-screen">
     <Layout>
-
       <FilterCategories />
       <CardsBoard />
     </Layout>
+    </div>
   );
 }
