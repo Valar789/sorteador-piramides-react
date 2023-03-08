079561f2f7b332b9313cd2644e0fdef9284765b0
angimarcela <a.marcela.m.j@gmail.com>
2023-03-08 16:39:54 -0500

    header tamplates

--- src/views/Templates.jsx
+++ src/views/Templates.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import BreadCrumb from "../components/layout/BreadCrumb";
 import Header from "../components/layout/Header";
 import TableCategories from "../components/layout/pyramid/TableCategories";
 import TablePositions from "../components/layout/pyramid/TablePositions";
@@ -7,10 +8,12 @@ import HeaderTemplates from "../components/templates-pyramids/HeaderTemplates";
 export default function Templates() {
   return (
     <>
-      <Header />
-      <HeaderTemplates/>
-      <TableCategories />
-      <TablePositions />
+      <div id="" className="px-32 pt-10 text-white ">
+        <BreadCrumb/>
+        <HeaderTemplates />
+        <TableCategories />
+        <TablePositions />
+      </div>
     </>
   );
 }
