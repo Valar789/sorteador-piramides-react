66be50be0cccdb5cd4eab0c74804a2aecd68ad14
Valar789 <luismzo789@gmail.com>
2023-03-08 10:30:19 -0500

    add function obtenerPropiedadesUnicas for selects filters

--- src/components/board/SelectFilter.jsx
+++ src/components/board/SelectFilter.jsx
@@ -8,17 +8,43 @@ const selectFilters = [
   "Estado",
 ];
 export default function SelectFliter() {
-  const [data, setdata] = useState(localStorage.getItem("excelData"))
+  const [data, setData] = useState([])
+  const [valuesSelect, setValuesSelect] = useState({})
 
+useEffect(() => {
+  const dataFromLocalStorage = JSON.parse(localStorage.getItem("excelData"));
+  setData(dataFromLocalStorage);
+  
+  //recibe data (json) y retorna un objeto 
+  function obtenerPropiedadesUnicas() {
+    let propiedades = {};
+    dataFromLocalStorage.forEach((obj) => {
+      for (let key in obj) {
+        if (key !== "CodDep" && key !== "Nombre Deportista" && key !== "Doc. Identificación") {
+          let value = obj[key];
+          if (Array.isArray(propiedades[key])) {
+            if (!propiedades[key].includes(value)) {
+              propiedades[key].push(value);
+            }
+          } else {
+            propiedades[key] = [value];
+          }
+        }
+      }
+    });
+    return propiedades;
+  }
+  const res = obtenerPropiedadesUnicas()
+  setValuesSelect(res)
+  
+}, [])
 
-  const keyRama = data.map(e=>e['Rama']);
-  const ramaIndividual = keyRama.filter((e, i )=> keyRama.indexOf(e) === i )
+console.log(valuesSelect);
 
 
   return (
     <div className="flex items-center justify-center min-w-full mb-16">
       <div className="flex flex-col ">
-       
         <div className="flex">
           {selectFilters.map((filter, i) => (
             <div key={i} className="flex flex-col mx-5">
